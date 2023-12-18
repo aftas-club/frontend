6b6e363ae0e0b8b701c73192b4ecf8904acd7b3a
ouharri <ouharrioutman@gmail.com>
2023-12-18 20:21:52 +0100

    app config

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {TuiRootModule} from "@taiga-ui/core";
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      CommonModule,
+      HttpClientModule,
+      TuiRootModule
+    ),
+    provideAnimations(),
+    provideRouter(routes),
+    provideClientHydration()
+  ]
+};
